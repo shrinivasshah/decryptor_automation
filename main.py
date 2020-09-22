a0d1df435a63f96486032be1c17a3a95ccf6d681
Shrinivas <shrinivas@pop-os.localdomain>
2020-09-22 11:21:18 +0530

    killa bee

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import pyautogui
+import time
 # import datetime
 
 pag = pyautogui
 
-print(pag.position())
-pag.moveTo(x=193,y=223)
+# print(pag.position())
+pag.moveTo(x=163,y=229, duration=0.7)
 pag.click()
 pag.hotkey('ctrl','c')
-pag.moveTo(x=596,y=339)
+pag.moveTo(x=698, y=449, duration=0.7)
+pag.click()
 pag.hotkey('ctrl','a')
 pag.hotkey('ctrl','v')
-pag.moveTo(x=618, y=407)
+pag.moveTo(x=746, y=492, duration=0.7)
 pag.click()
-pag.moveTo(x=485, y=438)
+pag.moveTo(x=600, y=544,  duration=0.7)
+pag.click()
+pag.hotkey('ctrl','a')
+pag.hotkey('ctrl','c')
+pag.moveTo(x=1017, y=254, duration=0.7)
+pag.hotkey('ctrl','v')
 pag.press('down')
+
 
 
 
