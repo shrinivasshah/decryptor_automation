84c700cb7d101611f910e524fc30d74e3128d84a
shrinivasshah <shrinivasshah05@gmail.com>
2020-09-28 11:52:18 +0530

    last

--- main.py
+++ main.py
@@ -1,33 +1,97 @@
 import pyautogui
 import time
+import pyperclip
 # import datetime
 
 pag = pyautogui
+count = 0
 
-# print(pag.position())
-pag.moveTo(x=163,y=229, duration=0.7)
+
+def copy_clipboard():
+    pag.hotkey('ctrl','c')
+    time.sleep(0.1)
+    
+    return str(pyperclip.paste())
+
+
+
+time.sleep(9)
+pag.moveTo(x=1033, y=242, duration=0.5)
 pag.click()
-pag.hotkey('ctrl','c')
-pag.moveTo(x=491, y=452, duration=0.7)
-pag.click()
-pag.keyDown('delete')
+for i in range(29021):
+    pag.hotkey('ctrl','c')
+    time.sleep(0.1)
+    x = str(pyperclip.paste())
+    if x != '':      
+        pag.hotkey('alt','tab')
+        time.sleep(.3)
+        pag.hotkey('ctrl','a')
+        pag.hotkey('ctrl','v')
+        pag.press('tab', presses=3)
+        time.sleep(.3)
+        pag.press('enter')
+        time.sleep(.3)
+        pag.press('tab', presses=2)
+        pag.hotkey('ctrl','a')
+        pag.hotkey('ctrl','c')
+        pag.press('tab',presses=3)
+        pag.sleep(.3)
+        pag.hotkey('alt','tab')
+        time.sleep(.3)
+        pag.press('right')
+        pag.hotkey('ctrl','v')
+        pag.press('left')
+        pag.press('down')
+    else:
+        pag.press('down')
+# print(pag.posit ion())
+# pag.moveTo(x=1045, y=224, duration=0.7)
+# pag.click()
+# pag.hotkey('ctrl','c')
+# pag.moveTo(x=487, y=98, duration=0.7)
+# pag.click()
+# pag.hotkey('ctrl','a')
+# pag.hotkey('ctrl','v')
+# pag.moveTo(x=732, y=137, duration=0.7)
+# pag.click()
+# pag.moveTo(x=595, y=186,  duration=0.7)
+# pag.click()
+# pag.hotkey('ctrl','a')
+# pag.hotkey('ctrl','c')
+# pag.moveTo(x=1299, y=227, duration=0.7)
+# pag.click()
+# pag.hotkey('ctrl','v')
+# pag.press('down')
 
-pag.keyUp('delete')
-pag.hotkey('ctrl','v')
-pag.moveTo(x=746, y=508, duration=0.7)
-pag.click()
-pag.moveTo(x=493, y=545,  duration=0.7)
-pag.click()
-pag.drag(300,0)
-pag.hotkey('ctrl','c')
-pag.moveTo(x=1017, y=254, duration=0.7)
-pag.hotkey('ctrl','v')
-pag.press('down')
+
+
+
+# for i in range(29021):
+    
+#     pag.moveTo(x=1045, y=224, duration=0.7)
+#     pag.click()
+#     pag.press('down', presses=count)
+#     pag.hotkey('ctrl','c')
+#     pag.moveTo(x=487, y=98, duration=0.7)
+#     pag.click()
+#     pag.hotkey('ctrl','a')
+#     pag.hotkey('ctrl','v')
+#     pag.moveTo(x=732, y=137, duration=0.7)
+#     pag.click()
+#     pag.moveTo(x=595, y=186,  duration=0.7)
+#     pag.click()
+#     pag.hotkey('ctrl','a')
+#     pag.hotkey('ctrl','c')
+#     pag.moveTo(x=1299, y=227, duration=0.7)
+#     pag.click()
+#     pag.press('down',presses=count)
+#     pag.hotkey('ctrl','v')
+#     pag.press('down')
+#     count += 1
 
 
 
 
 
-
-
+print('')
 
